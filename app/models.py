176c746e5b66139b2fa6a9db297a7914bbbaffba
josseMolenschot <josse.molenschot@student.odisee.be>
2024-12-11 10:38:03 +0000

    patch

--- app/models.py
+++
@@ -1,6 +0,0 @@
-from . import db
-
-class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(150), nullable=False, unique=True)
-    password = db.Column(db.String(150), nullable=False)
